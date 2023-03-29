673953dad731d6673b2b9f24d6fe837d6f771f68
Ida Danielsson <ida.danielsson96@icloud.com>
2023-03-29 18:07:18 +0200

    commit

--- src/components/main/home/about/About.tsx
+++ src/components/main/home/about/About.tsx
@@ -12,14 +12,16 @@ export const About = () => {
           </div>
 
           <p className="about__description">
-            My name is Ida Danielsson, and I’m a first-year web development
-            student at Medieinstitutet in Stockholm. During my first year, I
-            established solid knowledge in both frontend development as well as
-            backend development. The program includes languages such as
-            JavaScript, typescript, react, SQL, PHP, and various frameworks. For
-            the past years, I have mastered a bachelor’s degree in media and
-            communications, followed by almost two years working at one of
-            Sweden’s fastest-growing e-commerce.
+            My name is Ida Danielsson! I’m an ambitious and creative web
+            developer student specializing in e-commerce, currently living in
+            Uppsala. During my first year at Medieinstitutet in Stockholm, I
+            have established solid knowledge in both frontend and backend
+            development with skills in HTML, CSS/SCSS, JavaScript, TypeScript,
+            React, SQL, PHP, and APIs. <br></br>
+            <br></br>I have always been passionate about creating in various
+            forms and for the past years, I have mastered a bachelor’s degree in
+            media and communications, followed by almost two years working at
+            one of Sweden’s fastest-growing e-commerce.
           </p>
           <Link to="/resume" className="about__btn">
             My Resume
